85056f7dc299afac7c22705eddb997f0449aea80
Dlt <71595451+MrDlt@users.noreply.github.com>
2023-03-11 20:09:20 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+import openai
+openai.api_key="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"
+openai.Completion.create( 
+  engine="davinci",
+  prompt="Make a list of astronomical observatories:"
+)
